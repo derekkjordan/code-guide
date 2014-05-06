d6cb5d06c2ea17dcab99137ae8dfd112425a423f
celamatthews <marcela.m@quantumrewards.com>
2014-05-06 11:28:50 -0700

    footer changes

---
+++ _includes/js/spacing.js
@@ -0,0 +1,23 @@
+var i = 0;
+ 
+if ( condition ) {
+    doSomething();
+} else if ( otherCondition ) {
+    somethingElse();
+} else {
+    otherThing();
+}
+ 
+while ( !condition ) {
+    iterating++;
+}
+ 
+for ( ; i < 100; i++ ) {
+    object[ array[ i ] ] = someFn( i );
+}
+ 
+try {
+    // Expressions
+} catch ( e ) {
+    // Expressions
+}
